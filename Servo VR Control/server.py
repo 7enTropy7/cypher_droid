3049c6929249e123c837eb5bb429df06dd5d84fc
7enTropy7 <menon.uk1998@gmail.com>
2021-03-25 17:45:45 +0530

    for testing

--- Servo VR Control/server.py
+++ Servo VR Control/server.py
@@ -1,4 +1,46 @@
 from flask import Flask, render_template_string, request
+from pygame import *
+import RPi.GPIO as GPIO
+from time import sleep as Sleep
+
+GPIO.setwarnings(False)
+GPIO.setmode(GPIO.BOARD)
+
+# Servo Control Pins
+horizontal = 3
+vertical = 5
+
+GPIO.setup(horizontal, GPIO.OUT)
+pwm_h=GPIO.PWM(horizontal, 50)
+GPIO.setup(vertical, GPIO.OUT)
+pwm_v=GPIO.PWM(vertical, 50)
+pwm_h.start(0)
+pwm_v.start(0)
+
+
+def translate(value, leftMin, leftMax, rightMin, rightMax):
+    leftSpan = leftMax - leftMin
+    rightSpan = rightMax - rightMin
+    valueScaled = float(value - leftMin) / float(leftSpan)
+    return rightMin + (valueScaled * rightSpan)
+
+
+def SetAngle(angle,target_pin):
+    if target_pin == 3:
+        duty = angle / 18 + 2
+        GPIO.output(target_pin, True)
+        pwm_h.ChangeDutyCycle(duty)
+        Sleep(0.2)
+        GPIO.output(target_pin, False)
+        pwm_h.ChangeDutyCycle(0)
+    elif target_pin == 5:
+        duty = angle / 18 + 2
+        GPIO.output(target_pin, True)
+        pwm_v.ChangeDutyCycle(duty)
+        Sleep(0.2)
+        GPIO.output(target_pin, False)
+        pwm_v.ChangeDutyCycle(0)
+
 
 TPL = '''
 <html>
@@ -49,10 +91,16 @@ def serveRoot():
 
 @app.route("/moveservos", methods=["POST"])
 def moveServos():
-    vertical = request.form["updown"]
-    horizontal = request.form["leftright"]
-    print('Horizontal: ' + str(horizontal) + "        Vertical: " + str(vertical))
+    v_angle = request.form["updown"]
+    h_angle = request.form["leftright"]
+    print('Horizontal: ' + str(h_angle) + "        Vertical: " + str(v_angle))
+    SetAngle(v_angle,vertical)
+    SetAngle(h_angle,horizontal)
+    Sleep(0.2)
     return ""
+
+GPIO.cleanup()
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0")#, ssl_context='adhoc')
+
