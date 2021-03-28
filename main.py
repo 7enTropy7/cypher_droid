f869fed5303b1ae72ded4c755fb0949c9c2ede5f
7enTropy7 <menon.uk1998@gmail.com>
2021-03-28 20:23:07 +0530

    testing

--- main.py
+++ main.py
@@ -47,10 +47,10 @@ TPL = '''
 <body>
     <center><h1>PiCam Stream</h1></center>
     <div style="width:50%;Text-align:left;float:left;">
-        <img  class="camera-bg" style="width: 50%; height:50%; background-attachment: fixed;" id="bg" class="left" src="{{ url_for('video_feed') }}">        
+        <img  class="camera-bg" style="width: 70%; height:70%; background-attachment: fixed;" id="bg" class="center" src="{{ url_for('video_feed') }}">        
     </div>
     <div style="Text-align:right;Width:50%;float:right">
-        <img  class="camera-bg" style="width: 50%; height:50%; background-attachment: fixed;" id="bg" class="right" src="{{ url_for('video_feed') }}">        
+        <img  class="camera-bg" style="width: 70%; height:70%; background-attachment: fixed;" id="bg" class="center" src="{{ url_for('video_feed') }}">        
     </div>
         <button id="connectbtn" onclick="requestPermission()">Connect</button>
         <button id="calibratebtn" onclick="calibrate()">Calibrate</button>
