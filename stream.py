49cbeffd66752ef6a16121c52da9f8e850a13d7a
7enTropy7 <menon.uk1998@gmail.com>
2021-03-28 20:20:04 +0530

    video capture

---
+++ stream.py
@@ -0,0 +1,27 @@
+import cv2
+from imutils.video.pivideostream import PiVideoStream
+import imutils
+import time
+import numpy as np
+from flask import Flask, render_template, Response, request
+import threading
+import os
+
+class VideoCamera(object):
+    def __init__(self, flip = True):
+        self.vs = PiVideoStream().start()
+        self.flip = flip
+        time.sleep(2.0)
+
+    def __del__(self):
+        self.vs.stop()
+
+    def flip_if_needed(self, frame):
+        if self.flip:
+            return np.flip(frame,0)
+        return frame
+
+    def get_frame(self):
+        frame = self.flip_if_needed(self.vs.read())
+        ret, jpeg = cv2.imencode('.jpg', frame)
+        return jpeg.tobytes()
